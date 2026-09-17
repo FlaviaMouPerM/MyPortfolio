9166c94a696f804bd20b9432796661349b31d0fa
Flavia Martins <flaviamourapmartins@gmail.com>
2026-09-17 17:15:00 +0100

    Fix paths for GitHub Pages

--- src/main.js
+++ src/main.js
@@ -1,13 +1,11 @@
-import { initPortfolioSwitcher } from '/src/scripts/portfolioSwitcher.js';
-import { initLangSwitcher } from '/src/scripts/langSwitcher.js';
-import { initThemeSwitcher } from '/src/scripts/themeSwitcher.js';
-import { initNavigation } from '/src/scripts/navigation.js';
+import { initPortfolioSwitcher } from "./src/scripts/portfolioSwitcher.js";
+import { initLangSwitcher } from "./src/scripts/langSwitcher.js";
+import { initThemeSwitcher } from "./src/scripts/themeSwitcher.js";
+import { initNavigation } from "./src/scripts/navigation.js";
 
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   initPortfolioSwitcher();
   initThemeSwitcher();
   initLangSwitcher();
   initNavigation();
-  
 });
--- src/scripts/langSwitcher.js
+++ src/scripts/langSwitcher.js
@@ -1,26 +1,26 @@
-import { translations } from '/src/scripts/translations.js';
+import { translations } from "./translations.js";
 
 export function initLangSwitcher() {
-  const langBtn = document.getElementById('btn-lang-toggle');
-  const labelLang = document.getElementById('label-lang');
+  const langBtn = document.getElementById("btn-lang-toggle");
+  const labelLang = document.getElementById("label-lang");
 
-  const savedLang = localStorage.getItem('lang') || 'pt';
+  const savedLang = localStorage.getItem("lang") || "pt";
   applyLanguage(savedLang);
 
-  langBtn.addEventListener('click', () => {
-    const currentLang = document.documentElement.getAttribute('data-lang');
-    const newLang = currentLang === 'pt' ? 'en' : 'pt';
+  langBtn.addEventListener("click", () => {
+    const currentLang = document.documentElement.getAttribute("data-lang");
+    const newLang = currentLang === "pt" ? "en" : "pt";
     applyLanguage(newLang);
-    localStorage.setItem('lang', newLang);
+    localStorage.setItem("lang", newLang);
   });
 
   function applyLanguage(lang) {
-    document.documentElement.setAttribute('data-lang', lang);
+    document.documentElement.setAttribute("data-lang", lang);
     labelLang.textContent = lang.toUpperCase();
 
-    const elementsToTranslate = document.querySelectorAll('[data-i18n]');
-    elementsToTranslate.forEach(el => {
-      const key = el.getAttribute('data-i18n');
+    const elementsToTranslate = document.querySelectorAll("[data-i18n]");
+    elementsToTranslate.forEach((el) => {
+      const key = el.getAttribute("data-i18n");
       if (translations[lang] && translations[lang][key]) {
         el.innerText = translations[lang][key];
       }
