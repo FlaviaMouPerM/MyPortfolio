9dd967a112a5fc567693a09adbbeaddde29d6dc4
Flavia Martins <flaviamourapmartins@gmail.com>
2026-09-17 21:46:39 +0100

    Ajusta visual do modern hero

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import { initPortfolioSwitcher } from "./src/scripts/portfolioSwitcher.js";
-import { initLangSwitcher } from "./src/scripts/langSwitcher.js";
-import { initThemeSwitcher } from "./src/scripts/themeSwitcher.js";
-import { initNavigation } from "./src/scripts/navigation.js";
+import { initPortfolioSwitcher } from "./scripts/portfolioSwitcher.js";
+import { initLangSwitcher } from "./scripts/langSwitcher.js";
+import { initThemeSwitcher } from "./scripts/themeSwitcher.js";
+import { initNavigation } from "./scripts/navigation.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   initPortfolioSwitcher();
